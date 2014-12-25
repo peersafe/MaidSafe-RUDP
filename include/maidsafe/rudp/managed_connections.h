aac160b05a99c6292258c8eeee21064b93882b57
Liu Liu <shaxunyeman@gmail.com>
2014-12-26 03:46:50 +0800

    change kMaxMessageSize

--- include/maidsafe/rudp/managed_connections.h
+++ include/maidsafe/rudp/managed_connections.h
@@ -90,7 +90,7 @@ class ManagedConnections {
   ManagedConnections();
   ~ManagedConnections();
 
-  static int32_t kMaxMessageSize() { return 2097152; }
+  static int32_t kMaxMessageSize() { return 2097152 * 5; }
   static unsigned short kResiliencePort() { return kLivePort; }  // NOLINT (Fraser)
 
   // Creates a new transport object and bootstraps it to one of the provided bootstrap_endpoints.
